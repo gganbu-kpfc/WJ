a869b95ce2b8e2291bc6fad6e2a38c714f5e813d
gganbu-kpfc <gganbupartners@gmail.com>
2026-03-27 01:35:44 +0900

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://pxyykaduivfnhulpljai.supabase.co";
 const SUPABASE_KEY = "sb_publishable_TX186xuxH90gDZV22bQq8w_7XcubEqw";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
