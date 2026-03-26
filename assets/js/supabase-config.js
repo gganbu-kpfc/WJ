eb437cc96963e59ca74b216a3a59ac57211da9c7
gganbu-kpfc <gganbupartners@gmail.com>
2026-03-27 00:46:17 +0900

    Create supabase-config.js

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://pxyykaduivfnhulpljai.supabase.co";
+const SUPABASE_KEY = "여기에 Publishable Key 붙여넣기";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
